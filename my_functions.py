e6750397cc720e08350f00625a53a70b002c9b08
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2020-08-02 23:52:11 +0200

    my changes

--- my_functions.py
+++ my_functions.py
@@ -2,7 +2,19 @@ def gcd(a,b):
   """
   Find the GCD of 2 numbers
   """
+  #print "starts here"
   while b:
+    #print "inside loop 1 b: {} a: {}".format(b,a)
     a, b = b, a%b
+    #print "inside loop 2 b: {} a: {}".format(b,a)
 
   return a
+
+
+def lcm(a,b):
+  """
+  Find the LCM of 2 numbers
+  """
+
+  print "LCM starts here"
+  return a // gcd(a,b) * b
--- testing.py
+++ testing.py
@@ -4,13 +4,29 @@ import my_functions
 if __name__ == "__main__":
   a = 10
   b = 2
-  print "gcd {},{} {}".format(a,b,my_functions.gcd(a,b))
+  print "gcd {},{}: {}".format(a,b,my_functions.gcd(a,b))
   a = 101
   b = 201
-  print "gcd {},{} {}".format(a,b,my_functions.gcd(a,b))
+  print "gcd {},{}: {}".format(a,b,my_functions.gcd(a,b))
   a = 1000
   b = 500
-  print "gcd {},{} {}".format(a,b,my_functions.gcd(a,b))
-  print "gcd {},{} {}".format(a,b,my_functions.gcd(a,b))
-  print "gcd {},{} {}".format(a,b,my_functions.gcd(a,b))
-  print "gcd {},{} {}".format(a,b,my_functions.gcd(a,b))
+  print "gcd {},{}: {}".format(a,b,my_functions.gcd(a,b))
+  a = 1024
+  b = 2048
+  print "gcd {},{}: {}".format(a,b,my_functions.gcd(a,b))
+  # --------------------
+  a = 10
+  b = 2
+  print "lcm {},{}: {}".format(a,b,my_functions.lcm(a,b))
+  a = 101
+  b = 201
+  print "lcm {},{}: {}".format(a,b,my_functions.lcm(a,b))
+  a = 1000
+  b = 500
+  print "lcm {},{}: {}".format(a,b,my_functions.lcm(a,b))
+  a = 1024
+  b = 2048
+  print "lcm {},{}: {}".format(a,b,my_functions.lcm(a,b))
+  a = 1025
+  b = 2045
+  print "lcm {},{}: {}".format(a,b,my_functions.lcm(a,b))
